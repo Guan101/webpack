62971d3f715de5491ceb04f5451bc6009a9f7b3a
guanwenhao <407818306@qq.com>
2023-03-11 16:01:28 +0800

    update at 2023-03-11 16:01:28

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
       lang: "",
       title: "webpack",
       description:
-        "VuePress模板，主题，插件演示。展示了一个简单的VuePress案例模板，用户可以直接clone这个仓库，作为初始化一个VuePress网站启动项目，在这个项目的基础上新增自定义配置和功能。",
+        "",
     },
   },
   plugins: [
