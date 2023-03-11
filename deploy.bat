23a3a75c869f3c930051a0c00d63f5d140dbfdc6
guanwenhao <407818306@qq.com>
2023-03-11 15:18:22 +0800

    update at 2023-03-11 15:18:22

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,9 @@
 # deploy page
+
+ git add .
+ git commit -m "update at $time"
+ git push -f git@github.com:Guan101/webpack.git note
+
 npm run build
 git add docs/.vuepress/dist
 git commit -m "Initial dist subtree commit"
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ time=$(date "+%Y-%m-%d %H:%M:%S")
 
  git add .
  git commit -m "update at $time"
- git push -f git@github.com:Guan101/ts.git note
+ git push -f git@github.com:Guan101/webpack.git note
 # 打包生成静态文件
 npm run build
 
